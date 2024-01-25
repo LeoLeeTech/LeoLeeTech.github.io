e04336b497b90047e76b7f32c7650af071364bf0
lijilei <lijileigood@gmail.com>
2024-01-25 17:18:55 +0800

    master -> main

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 set -e
 
 # 生成静态文件
-npm run docs:build
+sudo npm run docs:build
 
 # 进入生成的文件夹
 cd docs/.vuepress/dist
@@ -16,6 +16,6 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:lijileiGood/lijileiGood.github.io.git master:gh-pages
+git push -f git@github.com:lijileiGood/lijileiGood.github.io.git main:gh-pages
 
 cd -
