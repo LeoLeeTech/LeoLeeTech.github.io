cad0f88a10449b8b81b673947e1e9884acef9372
Leo Lee <LeoLeeTech@gmail.com>
2026-04-27 16:07:37 +0800

    test

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,9 +1,9 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
-  { href: '/blog', title: 'Blog' },
-  { href: '/tags', title: 'Tags' },
-  { href: '/projects', title: 'Projects' },
-  { href: '/about', title: 'About' },
+  { href: '/blog', title: '文章' },
+  { href: '/tags', title: '分类' },
+  // { href: '/projects', title: 'Projects' },
+  { href: '/about', title: '关于我' },
 ]
 
 export default headerNavLinks
