d6b7aed7b42e551ce083e297e831c60f86ce54ec
Leo Lee <LeoLeeTech@gmail.com>
2026-04-28 10:26:24 +0800

    test

--- app/blog/[...slug]/page.tsx
+++ app/blog/[...slug]/page.tsx
@@ -7,18 +7,14 @@ import { MDXLayoutRenderer } from 'pliny/mdx-components'
 import { sortPosts, coreContent, allCoreContent } from 'pliny/utils/contentlayer'
 import { allBlogs, allAuthors } from 'contentlayer/generated'
 import type { Authors, Blog } from 'contentlayer/generated'
-import PostSimple from '@/layouts/PostSimple'
 import PostLayout from '@/layouts/PostLayout'
-import PostBanner from '@/layouts/PostBanner'
 import { Metadata } from 'next'
 import siteMetadata from '@/data/siteMetadata'
 import { notFound } from 'next/navigation'
 
 const defaultLayout = 'PostLayout'
 const layouts = {
-  PostSimple,
   PostLayout,
-  PostBanner,
 }
 
 export async function generateMetadata(props: {
